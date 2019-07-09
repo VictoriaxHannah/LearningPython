fa2d42999daf60bcab3ec9fbdbb8cf722b618fe5
Victoria Clark <33257986+VictoriaxHannah@users.noreply.github.com>
2019-07-09 09:20:39 +0100

    Update primenumbers.py
    Using a function

--- primenumbers.py
+++ primenumbers.py
@@ -2,13 +2,16 @@ small = int(input("Minumum number for range: "))
 large = int(input("Maximum number for range: "))
 primes = []
 
-for n in range(small,large + 1):
-   if n > 1:
-       for i in range(2,n):
-           if (n % i) == 0:
-               break
-       else:
-           primes.append(n)
+def Prime(n):
+    for n in range(small,large + 1):
+       if n > 1:
+           for i in range(2,n):
+               if (n % i) == 0:
+                   break
+           else:
+               primes.append(n)
 
+Prime(large)
+ 
 print("\nThere are",len(primes),"prime numbers between",small,"and",large)
 print("The numbers are:",primes)
